dbf707c563cdbabc0ec3c9128d1e11345ab72e3c
Jeon Eonseok <jseokeon@gmail.com>
2024-04-09 02:00:46 +0900

    design: main section 틀 구현

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { PropsWithChildren } from 'react';
 import Header from './header';
 import Footer from './Footer';
+import styled from '@emotion/styled';
 
 /** main layout */
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <Header />
-      {children}
+      <Main>{children}</Main>
       <Footer />
     </>
   );
 }
+
+const Main = styled.main`
+  max-width: 144rem;
+  width: 100%;
+  margin: 0 auto;
+`;
