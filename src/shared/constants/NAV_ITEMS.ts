cf6e9eef7708f2feca2b984afc7cd4baef101766
Jeon Eonseok <jseokeon@gmail.com>
2024-04-09 18:48:46 +0900

    feat: navigation 링크 추가 및 내용 수정

--- src/shared/constants/NAV_ITEMS.ts
+++ src/shared/constants/NAV_ITEMS.ts
@@ -1,12 +1,16 @@
 export const NAV_ITEMS = [
   {
     label: '전체',
+    link: '#post-preview-list',
   },
   {
     label: '코딩',
     subItems: ['TIL', 'Trouble shooting'],
+    link: '#',
   },
   {
     label: '일상',
+    subItems: ['생각정리', '회고'],
+    link: '#',
   },
 ];
