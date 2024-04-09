e20787e53365cf93ef0c416183d8fa90f9ee5610
Jeon Eonseok <jseokeon@gmail.com>
2024-04-09 12:46:03 +0900

    feat: SEO component 생성

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `blog`,
+    title: `Eonseok's blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
