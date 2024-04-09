19de304dbc918f6c7d9c7952f481a9b7e97e8f9b
Jeon Eonseok <jseokeon@gmail.com>
2024-04-09 16:53:57 +0900

    feat: 개별 post page 생성

---
+++ src/pages/{mdx.frontmatter__url}.tsx
@@ -0,0 +1,22 @@
+import { graphql } from 'gatsby';
+
+/** Post Detail Page */
+export default function Post({ data }) {
+  return (
+    <>
+      <p>{data.mdx.frontmatter.title}</p>
+      <p>{data.mdx.frontmatter.date}</p>
+    </>
+  );
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }
+`;
