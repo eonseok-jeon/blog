6d8a89759eea931f348aac5f244086e578755d14
Jeon Eonseok <jseokeon@gmail.com>
2024-04-28 17:48:41 +0900

    design: header logo 부분 내용 변경

---
+++ src/shared/components/layout/header/LeftSide.jsx
@@ -0,0 +1,94 @@
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+import IcSleepingMan from '@assets/icSleepingMan.svg';
+import IcWorkingMan from '@assets/icWorkingMan.svg';
+import { LEFT_NAV_ITEMS } from '../../../constants/LEFT_NAV_ITEMS';
+
+/** header left nav */
+function LeftNav() {
+  return (
+    <nav>
+      <NavList>
+        {LEFT_NAV_ITEMS.map(({ label, link }) => (
+          <Link to={link}>
+            <NavItem key={label}>{label}</NavItem>
+          </Link>
+        ))}
+      </NavList>
+    </nav>
+  );
+}
+
+const NavList = styled.ol`
+  display: flex;
+  gap: 4.8rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavItem = styled.li`
+  position: relative;
+  padding: 0.2rem 0;
+
+  &::before,
+  &::after {
+    position: absolute;
+    opacity: 0;
+    width: 0%;
+    height: 0.2rem;
+    content: '';
+    background: currentColor;
+    transition: all 0.3s;
+  }
+
+  &::before {
+    right: 0;
+    top: -0.2rem;
+  }
+
+  &::after {
+    left: 0;
+    bottom: 0;
+  }
+
+  &:hover::before,
+  &:hover::after {
+    opacity: 1;
+    width: 100%;
+  }
+`;
+
+/** header left side */
+export default function LeftSide() {
+  const randomNumber = Math.floor(Math.random() * 2);
+
+  return (
+    <Container>
+      <LogoWrapper to="/">
+        {randomNumber === 0 && <IcSleepingMan />}
+        {randomNumber === 1 && <IcWorkingMan />}
+      </LogoWrapper>
+      <LeftNav />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  gap: 5rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoWrapper = styled(Link)`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  cursor: pointer;
+
+  & svg {
+    width: 7rem;
+    height: 7rem;
+    border-radius: 50%;
+  }
+`;
---
+++ src/shared/constants/LEFT_NAV_ITEMS.js
@@ -0,0 +1,14 @@
+export const LEFT_NAV_ITEMS = [
+  {
+    label: 'Posts',
+    link: '#',
+  },
+  {
+    label: '이력서',
+    link: '#',
+  },
+  {
+    label: '포트폴리오',
+    link: '#',
+  },
+];
