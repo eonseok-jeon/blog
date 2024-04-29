37ab85ba87425ce537f798009821e4af583f0bc1
Jeon Eonseok <jseokeon@gmail.com>
2024-04-30 01:53:21 +0900

    chore: 나에 대한 소개글 작성

---
+++ src/views/mainPage/constants/ABOUT_ME.js
@@ -0,0 +1,8 @@
+export const ABOUT_ME = [
+  { text: '전언석입니다.', color: '#8e44ad' },
+  { text: '27살이에요.', color: '#BF625A' },
+  { text: '웹 개발을 해요.', color: '#c0392b' },
+  { text: '고양시에 살아요.', color: '#16a085' },
+  { text: '성장에 목말라 있어요.', color: '#2980b9' },
+  { text: '행복을 중요시해요.', color: '#F2A97E' },
+];
