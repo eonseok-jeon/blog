45476cea0839be05a5a3b5e95034ee3ed80bcb82
Jeon Eonseok <jseokeon@gmail.com>
2024-04-30 09:55:27 +0900

    feat: button click event tracking

--- src/shared/constants/LEFT_NAV_ITEMS.js
+++ src/shared/constants/LEFT_NAV_ITEMS.js
@@ -6,9 +6,11 @@ export const LEFT_NAV_ITEMS = [
   {
     label: '이력서',
     link: '/resume',
+    className: 'click-resume',
   },
   {
     label: '포트폴리오',
     link: '/portfolio',
+    className: 'click-portfolio',
   },
 ];
