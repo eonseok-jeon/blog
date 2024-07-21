caf123a768f1c98466a542ac7cd1a1a384c2808f
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-07-22 04:06:09 +0900

    [Design] 글자 간격 수정 (#32)
    * design: 글자 간격 수정 및 디자인 변경
    
    * docs: 변경된 디자인에 맞춰 posts 수정
    
    * chore: . 제거

--- src/shared/styles/theme.js
+++ src/shared/styles/theme.js
@@ -1,17 +1,28 @@
 export const lightTheme = {
   text: '#000',
-  lightText: '#999',
+  lightText: '#333',
+  strongText: '#000',
   background: '#fff',
   reverseBackground: '#121212',
-  codeBox: '#e5e7eb',
   linkColor: '#6363ff',
+  blockquoteBackground: '#f0f0f0',
+  codeBackground: '#eaeaea',
+  codeColor: '#ee5f5b',
+  preBackground: '#faf9f6',
+  h1Background: '#fcf4dd',
+  // fce1e4 ddedea daeaf6
 };
 
 export const darkTheme = {
   text: '#fff',
-  lightText: '#888',
+  lightText: '#ddd',
+  strongText: '#1faf2f',
   background: '#121212',
   reverseBackground: '#fff',
-  codeBox: '#55575b',
-  linkColor: '#6363ff',
+  linkColor: '#11cfff',
+  blockquoteBackground: '#2e2e2e',
+  codeBackground: '#aeaeae',
+  codeColor: '#ff1f1f',
+  preBackground: '#7e7e7e',
+  h1Background: '#5a5d60',
 };
