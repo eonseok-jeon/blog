2e5af9b79cfb5ab44dfdf308c7b551b6db9f3edb
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-08-15 15:38:20 +0900

    [Feat] SEO 개선 (#34)
    * feat: site metadata 추가
    
    * feat: SEO component 제작
    
    * feat: SEO component 적용
    
    * feat: og image 추가

---
+++ src/shared/components/SEO/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
+/** SEO */
+export default function SEO({
+  title,
+  description,
+  pathname,
+  ogImage,
+  children,
+}) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
+          ogImage
+        }
+      }
+    }
+  `);
+
+  const defaultTitle = data.site.siteMetadata.title;
+  const defaultDescription = data.site.siteMetadata.description;
+  const siteUrl = data.site.siteMetadata.siteUrl;
+  const defaultOgImage = data.site.siteMetadata.ogImage;
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname || ``}`,
+    ogImage: ogImage || defaultOgImage,
+  };
+
+  return (
+    <>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:image" content={seo.ogImage} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:site_name" content="eonseok's blog" />
+      <meta property="og:locale" content="ko_KR" />
+      <meta property="og:image:width" content="800" />
+      <meta property="og:image:height" content="400" />
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="author" content="Eonseok Jeon" />
+      {children}
+    </>
+  );
+}
