73e4d6d7ffd92d174c0ab50c3e82dda7c4e6b4e5
Jeon Eonseok <jseokeon@gmail.com>
2024-08-15 19:49:44 +0900

    docs: 이력서 제거

--- src/shared/constants/LEFT_NAV_ITEMS.js
+++ src/shared/constants/LEFT_NAV_ITEMS.js
@@ -3,11 +3,11 @@ export const LEFT_NAV_ITEMS = [
     label: 'Posts',
     link: '/posts',
   },
-  {
-    label: '이력서',
-    link: '/resume',
-    className: 'click-resume',
-  },
+  // {
+  //   label: '이력서',
+  //   link: '/resume',
+  //   className: 'click-resume',
+  // },
   // {
   //   label: '포트폴리오',
   //   link: '/portfolio',
